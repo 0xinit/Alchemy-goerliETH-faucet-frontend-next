13318f2a01728dd8b0f3a41e760ee46c77348f45
anmol <anmolpokhrel46@gmail.com>
2023-01-18 21:19:00 +0530

    added prettierrc

--- pages/api/getGoerli.js
+++ pages/api/getGoerli.js
@@ -1,31 +1,28 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import { ethers } from "ethers";
-const WALLET_PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY;
-const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
-const provider = new ethers.providers.AlchemyProvider(
-    "goerli",
-    ALCHEMY_API_KEY
-);
+import { ethers } from "ethers"
+const WALLET_PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY
+const provider = new ethers.providers.AlchemyProvider("goerli", ALCHEMY_API_KEY)
 
-const Wallet = new ethers.Wallet(WALLET_PRIVATE_KEY, provider);
+const Wallet = new ethers.Wallet(WALLET_PRIVATE_KEY, provider)
 
 export default async function sendEth(req, res) {
     try {
         if (req.method != "POST") {
-            return res.status(400).json({ message: "Please use post request" });
+            return res.status(400).json({ message: "Please use post request" })
         }
 
-        const walletAddress = req.body.address;
+        const walletAddress = req.body.address
         const txn = await Wallet.sendTransaction({
             to: walletAddress,
             value: ethers.utils.parseEther("0.000001"),
-        });
-        const receipt = await txn.wait(1);
+        })
+        const receipt = await txn.wait(1)
 
-        res.status(200).json({ message: "Transaction succesful!" });
+        res.status(200).json({ message: "Transaction succesful!" })
     } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: "Something went wrong!" });
+        console.error(err)
+        res.status(500).json({ message: "Something went wrong!" })
     }
 }
